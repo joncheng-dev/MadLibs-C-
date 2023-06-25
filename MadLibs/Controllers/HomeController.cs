f7078b4b85bd2085099888d56d34a23135c81f93
Jonathan Cheng <jcheng461@gmail.com>
2023-06-25 11:54:56 +0900

    update controller to use a model

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,25 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
   public class HomeController: Controller
   {
+    [Route("/")]
+    public ActionResult SplashPage()
+    {
+      return View();
+    }
+
+    [Route("/letter")]
+    public ActionResult MadLibsLetter()
+    {
+      InputForLetter userInputWords = new InputForLetter();
+      userInputWords.Recipient = "Homer";
+      return View(userInputWords);
+    }
+
     [Route("/hello")]
     public string Hello()
     {
       return "Hi there.";
-    }
-
-    [Route("/goodbye")]
-    public string Goodbye()
-    {
-      return "Goodbye.";
-    }
-
-    [Route("/")]
-    public string Letter()
-    {
-      return "MadLibs splash page here.";
     }
   }
 }
---
+++ MadLibs/Models/InputForLetter.cs
@@ -0,0 +1,9 @@
+namespace MadLibs.Models
+{
+  public class InputForLetter
+  {
+    public string Recipient { get; set; }
+    // public string NameOfPlace { get; set; }
+    // public string TopicToLearn { get; set; }
+  }
+}
